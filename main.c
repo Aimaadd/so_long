99c53f3b276bb86958f08dc35c8f5d8fd3461193
Aimaadd <aimadbentayeb@gmail.Com>
2024-02-17 17:00:57 +0100

    full screen

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abentaye <abentaye@student.s19.be >        +#+  +:+       +#+        */
+/*   By: abentaye <abentaye@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 13:46:32 by abentaye          #+#    #+#             */
-/*   Updated: 2024/02/14 17:50:33 by abentaye         ###   ########.fr       */
+/*   Updated: 2024/02/17 16:59:55 by abentaye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,6 @@
 #include <stdio.h>
 #include <limits.h>
 #include "so_long.h"
-
-int draw_line(void *mlx_ptr, void *mlx_win, int startX, int startY, int endX, int endY, int color)
-{
-    while (startX != endX || startX != endY)
-    {
-        mlx_pixel_put(mlx_ptr, mlx_win, startX, startY, 0x00FF00F0);
-        startX++;
-    }
-    mlx_loop(mlx_ptr);
-    return 1;
-}
 
 int	close_win(int keycode, t_win *var)
 {
@@ -54,40 +43,30 @@ int disp_color(int length, int width, t_win *mlg, int color)
     return 0;
 }
 
-// int render_next(void *astruct)
-// {
-//      while (color <= INT_MAX)
-//     {
-//         if (color == INT_MAX)
-//             color = 0;
-//         else
-//         {
-//             disp_color(length, width, mlg, color);
-//             color++;
-//         }
-//     } 
-// }
-
-int mouse_hi_bye(int keycode, t_win *var)
-{
-    if (keycode == ON_EXPOSE)
-        printf("hi\n");
-    if (keycode == ON_MOUSEUP)
-        printf("bye\n");
-    return 0;
-}
-
 void    put_images(t_win *mlg)
 {
     int len;
-
-    len = 32;
+    int x;
+    int y;
+    
+    y = 0;
+    x = 0;
+    len = 19173463;
     mlg->img.wall = mlx_xpm_file_to_image(mlg->win, CRATE, &len, &len);
     if (mlg->img.wall == NULL)
     {
         printf("raté\n");
     }
-    mlx_put_image_to_window(mlg->ptr, mlg->win, mlg->img.wall, len, len);
+    while (x <= 512 && y <= 512)
+    {
+        mlx_put_image_to_window(mlg->ptr, mlg->win, mlg->img.wall, x, y);
+        x += 64;
+        if (x == 512)
+        {
+            y += 64;
+            x = 0;
+        }
+    }
 }
 
 int main()
@@ -96,8 +75,8 @@ int main()
     char    *img_addr;
     int     x = 0;
     int     y = 0;
-    int     length = 720;
-    int     width = 480;
+    int     length = 512;
+    int     width = 512;
     int     color = 0;
     
     mlg.ptr = mlx_init();
